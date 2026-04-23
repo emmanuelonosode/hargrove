6ddd3ba5d3f82f19ef82fbb29c8993f2cf83c652
emmanuel onosode <emmanuelonosode4@gmail.com>
2026-04-23 02:28:55 +0100

    api issue

--- backend/scratch_check.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth import get_user_model
-User = get_user_model()
-for user in User.objects.all():
-    print(f"User: {user.email}, Role: {user.role}, Active: {user.is_active}")
